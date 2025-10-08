9102b0697b953e308137788fa93b5b34c8e3d5b4
Borja <borjagarciavillanueva@gmail.com>
2025-10-08 17:06:41 +0200

    fixed transform fucntion to transform a dearabic number to a roman one passing all the tests

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use InvalidArgumentException;
+
 class ArabicToRoman
 {
     /**
@@ -16,6 +18,35 @@ class ArabicToRoman
         $romanNumber = '';
 
         // Complete the function
+
+        if ($arabicNumber < 1 || $arabicNumber > 3999) {
+            throw new InvalidArgumentException('Number must be between 1 and 3999');
+        }
+
+        $map = [
+            'M'  => 1000,
+            'CM' => 900,
+            'D'  => 500,
+            'CD' => 400,
+            'C'  => 100,
+            'XC' => 90,
+            'L'  => 50,
+            'XL' => 40,
+            'X'  => 10,
+            'IX' => 9,
+            'V'  => 5,
+            'IV' => 4,
+            'I'  => 1,
+        ];
+
+        foreach ($map as $roman => $value) {
+            // Find how many times this symbol fits into the number
+            $count = intdiv($arabicNumber, $value);
+            if ($count) {
+                $romanNumber .= str_repeat($roman, $count);
+                $arabicNumber %= $value;
+            }
+        }
 
         return $romanNumber;
     }
