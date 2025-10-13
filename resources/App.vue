1694597a1e54de40051131ba0bc42297e4ba562e
Borja <borjagarciavillanueva@gmail.com>
2025-10-13 15:34:57 +0200

    added the table component that will draw the table wit the data

--- resources/App.vue
+++ resources/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    WORK HERE
+    <TableComponent :users="users" :propertyTypes="propertyTypes" :properties="properties" />
   </div>
 </template>
 
 <script>
 import { users, propertyTypes, properties } from '@/mocks/api';
+import TableComponent from '@/components/TableComponent.vue';
 
 export default {
   name: 'App',
+  components: {
+    TableComponent,
+  },
   data: () => ({
     users,
     propertyTypes,
