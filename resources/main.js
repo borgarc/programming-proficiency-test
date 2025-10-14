f2e3dd6cd5963406efc6528d9eabbc1d53f5934d
Borja <borjagarciavillanueva@gmail.com>
2025-10-14 21:52:41 +0200

    added tailwind

--- resources/main.js
+++ resources/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+const tailwind = document.createElement('script');
+tailwind.src = 'https://cdn.tailwindcss.com';
+document.head.appendChild(tailwind);
+
 Vue.config.productionTip = false;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+tailwind.onload = () => {
+  new Vue({
+    render: (h) => h(App),
+  }).$mount('#app');
+};
