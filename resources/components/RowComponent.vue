e2a783c3fc5a04c0d7fee8725020bd64a760c12a
Borja <borjagarciavillanueva@gmail.com>
2025-10-14 15:39:55 +0200

    rowComponent now renders the data

--- resources/components/RowComponent.vue
+++ resources/components/RowComponent.vue
@@ -1,13 +1,26 @@
 <template>
-  <div id="row-component">
-    Fila
-  </div>
+  <tr>
+    <td
+      class="border px-3 py-2"
+      v-for="(value, key) in row"
+      :key="key"
+    >
+    {{ value }}
+    </td>
+  </tr>
 </template>
 
 <script>
 
 export default {
   name: 'RowComponent',
+  props: {
+    row: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
   data: () => ({
   }),
 };
