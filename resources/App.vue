fdb021c07d30986134511ab0d0b91714a8e23be0
Borja <borjagarciavillanueva@gmail.com>
2025-10-14 21:54:02 +0200

    added component for user filter

--- resources/App.vue
+++ resources/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <SelectComponent
+      v-model="selectedUser"
+      :options="mapUsers"
+      placeholder="Filtro por usuarios"
+      label="Usuarios"
+    />
     <TableComponent
       :users="users"
       :propertyTypes="propertyTypes"
@@ -12,20 +18,32 @@
 <script>
 import { users, propertyTypes, properties } from '@/mocks/api';
 import TableComponent from '@/components/TableComponent.vue';
+import SelectComponent from '@/components/SelectComponent.vue';
 
 export default {
   name: 'App',
   components: {
     TableComponent,
+    SelectComponent,
   },
   data: () => ({
     users,
     propertyTypes,
     properties,
+    selectedUser: null,
     tableHeaders: [
-      'User', 'Property', 'Type', 'From', 'To', 'Duration',
-      'Currently Rented',
+      'Usuario', 'Propiedad', 'Tipo', 'Desde', 'Hasta', 'Duracion',
+      'Alquilado',
     ],
+    userList: [],
   }),
+  computed: {
+    mapUsers() {
+      return this.users.map((u) => ({ value: u.id, label: u.name }));
+    },
+    mapPropertyTypes() {
+      return this.propertyTypes.map((p) => ({ value: p.id, label: p.name }));
+    },
+  },
 };
 </script>
